789051092ae08ac85c5263c3b318fd5b2fd2019a
Nguyen Van Quang Huy <159224937+vanhuy2005@users.noreply.github.com>
2025-10-31 20:37:27 +0700

    Refactor Vite config to support command-based proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,17 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
-    proxy: {
-      '/api': {
-        // target: 'https://ffw-deploy-backend.onrender.com',
-        target: `http://localhost:${process.env.PORT_BE || 3000}`, // chỉnh lại localhost của backend
-        changeOrigin: true
-      }
-    }
-  }
-})
+    ...(command === 'serve' && {
+      proxy: {
+        '/api': {
+          target: `http://localhost:${process.env.PORT_BE || 3000}`,
+          changeOrigin: true,
+        },
+      },
+    }),
+  },
+}))
